ca4c83dd9b8d7e452fad77b704df7d0d586cced3
Vaibhav3M <44673587+Vaibhav3M@users.noreply.github.com>
2019-11-06 17:55:00 -0500

    Project Setup

---
+++ src/main/java/StarRentalApplication.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication(scanBasePackages={"com.sdm.CarRentalSystem"})
+public class StarRentalApplication {
+
+    public static void main(String[] args) {
+
+
+
+
+        SpringApplication.run(StarRentalApplication.class, args);
+    }
+}
+
