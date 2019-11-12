40a5c98e79a7c60e01e537e1ff12d05a6f0783ca
Vaibhav3M <44673587+Vaibhav3M@users.noreply.github.com>
2019-11-11 19:54:25 -0500

    added skeleton code for TDG

--- src/main/java/tableDataGateway/ITransactionTDG.java
+++ src/main/java/tableDataGateway/ITransactionTDG.java
@@ -1,4 +1,33 @@
 package tableDataGateway;
 
+import java.util.ArrayList;
+
+import model.Transaction;
+
 public interface ITransactionTDG {
+
+
+    public boolean createTransaction(int id, String vehicleLicencePlate,String clientLicenseNumber,String status,String timeStamp,String bookingFrom,String bookingTill,String transactionBy) throws Exception;
+
+    public Transaction getTransactionByID(int id) throws Exception;
+
+    public ArrayList<Transaction> getAllTransactions() throws Exception;
+    public ArrayList<Transaction> getTransactionForClient(String clientLicenseNumber) throws Exception;
+    public ArrayList<Transaction> getTransactionForVehicle(String vehicleLicencePlate) throws Exception;
+    public ArrayList<Transaction> getTransactionForDueDate(String bookingTill) throws Exception;
+    public ArrayList<Transaction> getTransactionForStatus(String status) throws Exception;
+    public ArrayList<Transaction> getTransactionForRentedTill(String bookingTill) throws Exception;
+    public ArrayList<Transaction> getTransactionForRentedFrom(String bookingFrom) throws Exception;
+    public ArrayList<Transaction> getTransactionForTwoCriteria(String val1, String val2, String criteria1,String criteria2) throws Exception;
+    public ArrayList<Transaction> getTransactionForThreeCriteria(String val1, String val2, String val3, String criteria1,String criteria2, String criteria3) throws Exception;
+    public ArrayList<Transaction> getTransactionForFourCriteria(String val1, String val2, String val3, String val4,String criteria1, String criteria2, String criteria3, String criteria4) throws Exception;
+    public ArrayList<Transaction> getTransactionForAllCriteria(String vehicleLicencePlate,String clientLicenseNumber,String status,String timeStamp,String bookingFrom,String bookingTill,String transactionBy, String criteria) throws Exception;
+
+
+    public ArrayList<Transaction> sortByStatus(ArrayList<Transaction> list);
+    public ArrayList<Transaction> sortByTransactiondate(ArrayList<Transaction> list);
+    public ArrayList<Transaction> sortBylicensePlate(ArrayList<Transaction> list);
+    public ArrayList<Transaction> sortByRentedTill(ArrayList<Transaction> list);
+    public ArrayList<Transaction> sortByRentedFrom(ArrayList<Transaction> list);
+    public ArrayList<Transaction> sortByClient(ArrayList<Transaction> list);
 }
--- src/main/java/tableDataGateway/IVehicleTDG.java
+++ src/main/java/tableDataGateway/IVehicleTDG.java
@@ -1,4 +1,28 @@
 package tableDataGateway;
 
-public class IVehicleTDG {
+import model.Vehicle;
+
+import java.util.ArrayList;
+
+public interface IVehicleTDG {
+
+
+    public boolean addNewVehicle(String type,int year, String model,String color,String licenseNumber, String status,String image) throws Exception;
+
+    public boolean deleteVehicle(String licenseNumber) throws Exception;
+
+    public boolean modifyVehicle(String type,int year, String model,String color,String licenseNumber, String status,String image) throws Exception;
+
+    public ArrayList<Vehicle> getAllVehicles() throws Exception;
+
+    public ArrayList<Vehicle> getVehicleFromOneCriteria(String value, String comparator, String criteria) throws Exception;
+
+    public  ArrayList<Vehicle> getVehicleFromTwoCriteria(String value1,String value2, String comparator, String criteria1, String criteria2) throws Exception;
+
+    public  ArrayList<Vehicle> getVehicleFromAllCriteria(String value1,String value2,String value3,String value4, String comparator, String criteria1, String criteria2,String criteria3,String criteria4) throws Exception;
+
+    public  ArrayList<Vehicle> getVehicleFromThreeCriteria(String value1,String value2,String value3, String comparator, String criteria1, String criteria2,String criteria3) throws Exception;
+
+    public Vehicle getVehicleByLicenseNo(String licenseNo) throws Exception;
+
 }
