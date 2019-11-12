b4061f2c04024d04f367b84590c1b196fe90dc16
arejea <adeyinka.areje@icloud.com>
2019-11-11 21:39:02 -0500

    ##error

--- src/main/java/tableDataGateway/IClientTDG.java
+++ src/main/java/tableDataGateway/IClientTDG.java
@@ -5,13 +5,5 @@ import java.util.ArrayList;
 import model.Client;
 
 public interface IClientTDG {
-	
-	public boolean addNewClient(String licenseNumber, String firstName, String lastName, String phoneNo, String licenseExpiryDate) throws Exception;
 
-	
-	public boolean deleteClient(String licenseNumber) throws Exception;
-	
-	public boolean updateClient(String licenseNumber, String firstName, String lastName, String phoneNo, String licenseExpiryDate) throws Exception;
-	
-	public ArrayList<Client> getAllClients() throws Exception;
 }
--- src/main/java/tableDataGateway/IUserTDG.java
+++ src/main/java/tableDataGateway/IUserTDG.java
@@ -6,42 +6,5 @@ import model.User;
 
 public interface IUserTDG {
 
-	/**
-	 * @param userName
-	 * @param userPass
-	 * @return
-	 */
-	public boolean addNewUser(String userName, String userPass, String name, String userType) throws SQLException, Exception;
-	
-	
-	/**
-	 * @param userName
-	 * @param userPass
-	 * @return
-	 */
-	public boolean deleteUser(String userName, String userPass) throws SQLException, Exception;
-	
-	
-	/**
-	 * @param userName
-	 * @param userPass
-	 * @param name
-	 * @param userType
-	 * @return
-	 * @throws SQLException
-	 * @throws Exception
-	 */
-	public boolean modifyUser(String userName, String userPass, String name, String userType) throws SQLException, Exception;
-	
-	
-	/**
-	 * @param userName
-	 * @param userPass
-	 * @return
-	 * @throws SQLException
-	 * @throws Exception
-	 */
-	public User authenticateUser(String userName, String userPass) throws SQLException, Exception;
-	
 	
 }
