e8ac40ffb7a910284d4c017eaa227ed3f9f89924
arejea <adeyinka.areje@icloud.com>
2019-11-11 20:44:12 -0500

    client methods in interface

--- src/main/java/tableDataGateway/IClientTDG.java
+++ src/main/java/tableDataGateway/IClientTDG.java
@@ -1,4 +1,17 @@
 package tableDataGateway;
 
-public class IClientTDG {
+import java.util.ArrayList;
+
+import model.Client;
+
+public interface IClientTDG {
+	
+	public boolean addNewClient(String licenseNumber, String firstName, String lastName, String phoneNo, String licenseExpiryDate) throws Exception;
+
+	
+	public boolean deleteClient(String licenseNumber) throws Exception;
+	
+	public boolean updateClient(String licenseNumber, String firstName, String lastName, String phoneNo, String licenseExpiryDate) throws Exception;
+	
+	public ArrayList<Client> getAllClients() throws Exception;
 }
