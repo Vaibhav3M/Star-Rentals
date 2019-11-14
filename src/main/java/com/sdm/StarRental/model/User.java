ba5d5dacb588a7d659078b3baaa2fd7183452b24
arejea <adeyinka.areje@icloud.com>
2019-11-14 15:37:16 -0500

    user authication TDG

--- src/main/java/com/sdm/StarRental/model/User.java
+++ src/main/java/com/sdm/StarRental/model/User.java
@@ -10,6 +10,8 @@ public class User {
     private String userName;
 
     private String password;
+    
+    private String name;
 
     /**
      * Instantiates a new User.
@@ -25,10 +27,11 @@ public class User {
      * @param userName the user name
      * @param password the password
      */
-    public User(String userType, String userName, String password) {
+    public User(String userType, String userName, String password, String name) {
         this.userType = userType;
         this.userName = userName;
         this.password = password;
+        this.name = name;
     }
 
     /**
@@ -83,5 +86,13 @@ public class User {
      */
     public void setPassword(String password) {
         this.password = password;
+    }
+    
+    public String getName(){
+    	return name;
+    }
+    
+    public void setName(String name) {
+    	this.name = name;
     }
 }
