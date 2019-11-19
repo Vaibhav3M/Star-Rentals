c0bba9aca463a0ce5575c39f1a601c35e50dbfea
Vaibhav3M <44673587+Vaibhav3M@users.noreply.github.com>
2019-11-19 02:10:43 -0500

    Added func: Clerk Main Page

--- src/main/java/com/sdm/StarRental/controller/clerkMainPageController.java
+++ src/main/java/com/sdm/StarRental/controller/clerkMainPageController.java
@@ -1,4 +1,24 @@
 package com.sdm.StarRental.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+@Controller
 public class clerkMainPageController {
+
+    @RequestMapping(value = "/clerkMainPage", method = RequestMethod.GET)
+    public String Main(@RequestParam Map<String, String> reqPar, ModelMap model, HttpSession httpSession) {
+        //	if (Utilities.validateSession(httpSession)) {
+
+        return "clerkMainPage";
+        //	} else {
+        //		return "unauthorized";
+        //	}
+    }
 }
