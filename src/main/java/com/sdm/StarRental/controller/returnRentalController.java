d7b9a4b3af4617035bc9ce09de9cbab4bdedc981
Vaibhav3M <44673587+Vaibhav3M@users.noreply.github.com>
2019-11-19 02:41:40 -0500

    Added func: Return Rental Page

--- src/main/java/com/sdm/StarRental/controller/returnRentalController.java
+++ src/main/java/com/sdm/StarRental/controller/returnRentalController.java
@@ -1,4 +1,145 @@
 package com.sdm.StarRental.controller;
 
+import com.sdm.StarRental.dataMapper.TransactionDM;
+import com.sdm.StarRental.dataMapper.VehicleDM;
+import com.sdm.StarRental.model.Transaction;
+import com.sdm.StarRental.model.Vehicle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.Map;
+
 public class returnRentalController {
+
+    private static Logger logger = LoggerFactory.getLogger(returnRentalController.class);
+
+
+    @Autowired
+    private TransactionDM transactionDM;
+    @Autowired
+    private VehicleDM vehicleDM;
+
+    ArrayList<Transaction> relatedTransactions = new ArrayList<>();
+
+
+    @RequestMapping(value = "/returnRental", method = RequestMethod.GET)
+    public String returnVehicle(@RequestParam Map<String, String> reqPar, ModelMap modelMap, HttpSession httpSession) throws Exception {
+
+//        ArrayList<Catalog> rentedVehicles = catalogService.getVehicleFromOneCriteria("Rented", null, "status");
+
+
+
+        ArrayList<Vehicle> rentedVehicles = new ArrayList<>();
+        for(Vehicle vehicle : vehicleDM.getAllVehicles()) {
+            if(vehicle.getStatus().equals("Rented")) {
+                rentedVehicles.add(vehicle);
+            }
+        }
+
+        relatedTransactions.clear();
+        for (Vehicle vehicle : rentedVehicles) {
+            ArrayList<Transaction> transactions = transactionDM.getTransactionForVehicleService(vehicle.getvehicleLicensePlate());
+
+            if (!transactions.isEmpty()) {
+                Transaction currTransaction = transactions.get(transactions.size() - 1);
+                if (currTransaction != null) {
+                    relatedTransactions.addAll(transactions);
+                }
+            }
+
+        }
+
+        if (relatedTransactions != null) {
+            modelMap.addAttribute("vehiclecatalogfound", "RESULT_FOUND");
+            modelMap.addAttribute("transactionsResults", relatedTransactions);
+        } else {
+            modelMap.addAttribute("transactionsResults", "No vehicle is rented at the moment!!");
+        }
+        return "returnRental";
+    }
+
+
+    @RequestMapping(value = "/searchreturnclient", method = RequestMethod.POST)
+    public String searchReturnVehicle(@RequestParam Map<String, String> reqParam, ModelMap modelMap, HttpSession httpSession) throws Exception {
+
+        ArrayList<Transaction> searchedTransactions = new ArrayList<>();
+
+        if (!reqParam.get("client_license").equals("") && reqParam.get("vehicle_licensePlate").equals("")) {
+
+            for (Transaction transaction : relatedTransactions) {
+                if (transaction.getClientLicenseNumber().equalsIgnoreCase(reqParam.get("client_license"))) {
+                    searchedTransactions.add(transaction);
+                }
+            }
+
+        } else if (reqParam.get("client_license").equals("") && !reqParam.get("vehicle_licensePlate").equals("")) {
+
+            for (Transaction transaction : relatedTransactions) {
+                if (transaction.getVehicleLicensePlate().equalsIgnoreCase(reqParam.get("vehicle_licensePlate"))) {
+                    searchedTransactions.add(transaction);
+                }
+
+            }
+
+        } else if (!reqParam.get("client_license").equals("") && !reqParam.get("vehicle_licensePlate").equals("")) {
+
+            for (Transaction transaction : relatedTransactions) {
+                if (transaction.getClientLicenseNumber().equalsIgnoreCase(reqParam.get("client_license")) && transaction.getVehicleLicensePlate().equalsIgnoreCase(reqParam.get("vehicle_licensePlate"))) {
+                    searchedTransactions.add(transaction);
+                }
+            }
+        }else if (reqParam.get("client_license").equals("") && reqParam.get("vehicle_licensePlate").equals("")) {
+
+            searchedTransactions.addAll(relatedTransactions);
+        }
+
+        if (!searchedTransactions.isEmpty()) {
+            modelMap.addAttribute("vehiclecatalogfound", "RESULT_FOUND");
+            modelMap.addAttribute("transactionsResults", searchedTransactions);
+        } else {
+            modelMap.addAttribute("vehiclecatalogfound", "RESULT_NOT_FOUND");
+            modelMap.addAttribute("transactionsResults", "Cannot find item!!");
+        }
+
+        return "returnRental";
+    }
+
+
+
+    @RequestMapping(value = "/returnClientForm",method = RequestMethod.POST)
+    public String deleteClientInfo(@RequestParam Map<String,String> reqParam, ModelMap model,HttpSession httpSession) throws Exception{
+
+        int transactionID = Integer.parseInt(reqParam.get("transactionID"));
+
+        for (int i=0;i<relatedTransactions.size();i++){
+            Transaction currTransaction = relatedTransactions.get(i);
+            if(currTransaction.getTransactionID() == transactionID){
+                transactionDM.createTransactionService(currTransaction.getVehicleLicensePlate(),"Rental" ,currTransaction.getClientLicenseNumber(),"Return","","NA","NA",currTransaction.getTransactionBy());
+                Vehicle vehicle = vehicleDM.getVehicleByLicenseNo(currTransaction.getVehicleLicensePlate());
+                vehicleDM.modifyVehicle(vehicle.getType(),vehicle.getMake(),vehicle.getModel(),vehicle.getYear(),vehicle.getColor(),vehicle.getvehicleLicensePlate(),"Available");
+                relatedTransactions.remove(i);
+            }
+        }
+
+
+
+        if (!relatedTransactions.isEmpty()) {
+            model.addAttribute("vehiclecatalogfound", "RESULT_FOUND");
+            model.addAttribute("transactionsResults", relatedTransactions);
+        } else {
+            model.addAttribute("vehiclecatalogfound", "RESULT_NOT_FOUND");
+            model.addAttribute("transactionsResults", "Cannot find item!!");
+        }
+
+
+        return "returnRental";
+    }
+
 }
