460bdebf2d7d298eaecb30a2db4fea4694f577d0
arejea <adeyinka.areje@icloud.com>
2019-11-19 19:27:15 -0500

    job scheduler for unit of work

---
+++ src/main/java/com/sdm/StarRental/JobScheduler.java
@@ -0,0 +1,21 @@
+package com.sdm.StarRental;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.sdm.StarRental.unitOfWork.VehicleUnitOfWork;
+
+@Component
+public class JobScheduler {
+
+	@Autowired
+	private VehicleUnitOfWork vehicleUnitOfWork;
+	
+	//fixedRate is in milliseconds
+	@Scheduled(fixedRate = 2000)
+	public void scheduleTaskWithFixedRate() {
+		System.out.println("Job Executed");
+		//vehicleUnitOfWork.commit();
+	}
+}
